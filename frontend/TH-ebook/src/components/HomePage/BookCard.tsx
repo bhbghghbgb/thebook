92755b4b6eca3b4c8f25c659ea9e0c5cbc5a9516
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2024-10-19 23:06:22 +0700

    Thêm HomePage

--- frontend/TH-ebook/src/components/HomePage/BookCard.tsx
+++ frontend/TH-ebook/src/components/HomePage/BookCard.tsx
@@ -7,6 +7,7 @@ const BookCard = ({book}: { book: Book }) => {
     return (
         <div className="relative overflow-hidden group">
             <div
+                id="book-info"
                 className="absolute top-0 left-0 w-full bg-black bg-opacity-50 text-white p-2 transition-transform transform group-hover:translate-y-[-10px]">
                 <h2 className="text-lg font-bold">{book.title}</h2>
                 <p className="text-sm">{
@@ -15,6 +16,7 @@ const BookCard = ({book}: { book: Book }) => {
                 <p className="text-sm">{book.published_year}</p>
             </div>
             <img
+                id="book-cover"
                 className="w-full h-auto block"
                 src={book.cover_image}
                 alt={book.title}
--- frontend/TH-ebook/src/main.tsx
+++ frontend/TH-ebook/src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import HungApp from './Book';
+// import HungApp from './Book';
 import './index.css';
+import HomePage from './pages/HomePage';
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HungApp />
+      <HomePage />
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
