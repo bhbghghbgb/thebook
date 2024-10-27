b1106cddf551cf03793f5db75d1ac45c0e115f73
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2024-10-27 12:08:15 +0700

    Sử dụng InformationContainer thay cho AuthorContainer

--- frontend/TH-ebook/src/components/BookDetail/BookDetail.tsx
+++ frontend/TH-ebook/src/components/BookDetail/BookDetail.tsx
@@ -1,92 +1,97 @@
-import {Book} from "../../models/Book.ts";
+import { Book } from "../../models/Book.ts";
 import CategoryContainer from "./CategoryContainer.tsx";
 import RatingsContainer from "./RatingsContainer.tsx";
 import AuthorContainer from "./AuthorContainer.tsx";
 import TitleContainer from "./TitleContainer.tsx";
 import ButtonGroupContainer from "./ButtonGroupContainer.tsx";
 import TabDefault from "./TabDefault.tsx";
+import InformationContainer from "./InformationContainer.tsx";
 
 interface BookDetailProps {
-    book: Book;
-    onAddToLibrary: () => void;
-    onPreview: () => void;
-    onPreoder: () => void;
-    onSub: () => void;
-    onCategoryClick: (categoryName: string) => void;
-    onAuthorClick: (authorName: string) => void;
+  book: Book;
+  onAddToLibrary: () => void;
+  onPreview: () => void;
+  onPreoder: () => void;
+  onSub: () => void;
+  onCategoryClick: (categoryName: string) => void;
+  onAuthorClick: (authorName: string) => void;
 }
 
 const BookDetail = ({
-                        book,
-                        onAddToLibrary,
-                        onPreview,
-                        onPreoder,
-                        onSub,
-                        onCategoryClick,
-                        onAuthorClick,
-                    }: BookDetailProps) => {
-    return (
-        <>
-            {/* Container */}
-            <div className="
+  book,
+  onAddToLibrary,
+  onPreview,
+  onPreoder,
+  onSub,
+  onCategoryClick,
+  onAuthorClick,
+}: BookDetailProps) => {
+  const authors = book.authors.map((author) => author.name);
+
+  return (
+    <>
+      {/* Container */}
+      <div
+        className="
             container grid px-4
             md:grid-areas-product-detail-mobile md:grid-cols-[200px_auto]
             grid-areas-product-detail grid-cols-[1fr_200px_minmax(0,calc(1240px-3.5rem))_1fr]
             w-full h-full m-0 p-0
-             ">
-                <div className="nav-l grid-in-cover mr-5">
-                    <img
-                        src={book.cover_image}
-                        alt=""
-                        className="w-52 h-80 object-cover"
-                    />
-                </div>
-                {/*Tile*/}
-                <div className="grid-in-title">
-                    <TitleContainer book={book}/>
-                </div>
+             "
+      >
+        <div className="nav-l grid-in-cover mr-5">
+          <img
+            src={book.cover_image}
+            alt=""
+            className="w-52 h-80 object-cover"
+          />
+        </div>
+        {/*Tile*/}
+        <div className="grid-in-title">
+          <TitleContainer book={book} />
+        </div>
 
-                {/*ButtonGroup*/}
-                <div className="grid-in-buttons sm:ml-2 relative">
-                    <ButtonGroupContainer
-                        onAddToLibrary={onAddToLibrary}
-                        onPreview={onPreview}
-                        onPreoder={onPreoder}
-                        onSub={onSub}
-                    />
-                </div>
+        {/*ButtonGroup*/}
+        <div className="grid-in-buttons sm:ml-2 relative">
+          <ButtonGroupContainer
+            onAddToLibrary={onAddToLibrary}
+            onPreview={onPreview}
+            onPreoder={onPreoder}
+            onSub={onSub}
+          />
+        </div>
 
-                {/* category */}
-                <div className="grid-in-info sm:mx-2">
-                    <CategoryContainer book={book} onCategoryClick={onCategoryClick}/>
-                </div>
+        {/* category */}
+        <div className="grid-in-info sm:mx-2">
+          <CategoryContainer book={book} onCategoryClick={onCategoryClick} />
+        </div>
 
+        {/* rating */}
+        <div className="grid-in-stats sm:mx-2 mt-auto sm:mt-0">
+          <RatingsContainer />
+        </div>
 
-                {/* rating */}
-                <div className="grid-in-stats sm:mx-2 mt-auto sm:mt-0">
-                    <RatingsContainer/>
-                </div>
+        {/* description */}
+        <div className="grid-in-synopsis min-w-0">
+          <p className="story-description">{book.description}</p>
+        </div>
 
-                {/* description */}
-                <div className="grid-in-synopsis min-w-0">
-                    <p className="story-description">{book.description}</p>
-                </div>
-
-                <div className="content flex gap-6 items-start grid-in-content">
-                    {/* Author */}
-                    <AuthorContainer book={book} onAuthorClick={onAuthorClick}/>
-                    {/* Parts */}
-                    <TabDefault/>
-                </div>
-            </div>
-
-
-        </>
-    );
+        <div className="content flex gap-6 items-start grid-in-content">
+          {/* Author */}
+          <InformationContainer
+            header="Author"
+            content={authors}
+            onContainerClick={onAuthorClick}
+          />
+          {/* Parts */}
+          <TabDefault />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BookDetail;
-
 
 /* 
 
