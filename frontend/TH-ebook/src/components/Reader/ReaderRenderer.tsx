e73f58bb40cd03bde4eb75d0c0fb1c69678c8b19
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2024-11-15 10:55:04 +0700

    fix page reader bi hien navbar 2 lan

--- frontend/TH-ebook/src/components/Reader/ReaderRenderer.tsx
+++ frontend/TH-ebook/src/components/Reader/ReaderRenderer.tsx
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import parse from "html-react-parser";
+import $ from "jquery";
+import DOMPurify from "dompurify";
 const ReaderRenderer = () => {
   const { data, error, isLoading } = useQuery<string>({
     queryKey: ["reader"],
     queryFn: () =>
-      axios.get("/iawwl.htm", { responseType: "text" }).then((res) => res.data),
+      axios
+        .get("/content/1/2-bctcqcbc.htm", { responseType: "text" })
+        .then((res) => res.data),
   });
   if (isLoading) {
     return <span>Loading...</span>;
@@ -13,7 +17,20 @@ const ReaderRenderer = () => {
   if (error) {
     return <span>Error: {error.message}</span>;
   }
-  return <div id="reader" className="w-full">{parse(data)}</div>;
+  return (
+    <div id="reader" className="w-full">
+      <Renderer data={data} />
+    </div>
+  );
 };
-
+const Renderer = ({ data }: { data: string | null | undefined }) => {
+  if (!data) return <>Cannot load</>;
+  // fix relative link to local files
+  const dom = $(DOMPurify.sanitize(data));
+  dom.find("[src]").attr("src", function () {
+    const src = $(this).attr("src");
+    if (!src?.startsWith("/content")) return `/content/1/${src}`;
+  });
+  return parse(dom.html());
+};
 export default ReaderRenderer;
--- frontend/TH-ebook/src/pages/ReaderPage.tsx
+++ frontend/TH-ebook/src/pages/ReaderPage.tsx
@@ -1,114 +1,111 @@
 import { IconButton } from "@material-tailwind/react";
 import { useEffect } from "react";
 import {
-  HiChevronLeft,
-  HiChevronDoubleLeft,
-  HiChevronRight,
-  HiChevronDoubleRight,
-  HiPlus,
-  HiMinus,
-  HiInformationCircle,
   HiBookmark,
+  HiChevronDoubleLeft,
+  HiChevronDoubleRight,
+  HiChevronLeft,
+  HiChevronRight,
+  HiInformationCircle,
+  HiMinus,
+  HiPlus,
 } from "react-icons/hi";
 import { TbSunMoon } from "react-icons/tb";
 import { useNavigate, useParams } from "react-router-dom";
-import LayoutComponent from "../components/Share/LayoutComponent.tsx";
 import ReaderRenderer from "../components/Reader/ReaderRenderer";
 
 function NavigationBar() {
   return (
-    <LayoutComponent isMobile={false}>
-      <div className="grid grid-cols-3 w-full h-16">
-        <div className="flex flex-row gap-4 place-items-center justify-start">
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            aria-label="mmb"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <TbSunMoon className="w-6 h-6"></TbSunMoon>
-          </IconButton>
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiBookmark className="w-6 h-6"></HiBookmark>
-          </IconButton>
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiInformationCircle className="w-6 h-6"></HiInformationCircle>
-          </IconButton>
-        </div>
-        <div className="flex flex-row gap-4 place-items-center justify-center">
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiChevronDoubleLeft className="w-6 h-6"></HiChevronDoubleLeft>
-          </IconButton>
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiChevronLeft className="w-6 h-6"></HiChevronLeft>
-          </IconButton>
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiChevronRight className="w-6 h-6"></HiChevronRight>
-          </IconButton>
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiChevronDoubleRight className="w-6 h-6"></HiChevronDoubleRight>
-          </IconButton>
-        </div>
-        <div className="flex flex-row gap-4 place-items-center justify-end">
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiMinus className="w-6 h-6"></HiMinus>
-          </IconButton>
-          <IconButton
-            color="deep-orange"
-            size="lg"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <HiPlus className="w-6 h-6"></HiPlus>
-          </IconButton>
-        </div>
+    <div className="grid grid-cols-3 w-full h-16">
+      <div className="flex flex-row gap-4 place-items-center justify-start">
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          aria-label="mmb"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <TbSunMoon className="w-6 h-6"></TbSunMoon>
+        </IconButton>
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiBookmark className="w-6 h-6"></HiBookmark>
+        </IconButton>
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiInformationCircle className="w-6 h-6"></HiInformationCircle>
+        </IconButton>
       </div>
-    </LayoutComponent>
+      <div className="flex flex-row gap-4 place-items-center justify-center">
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiChevronDoubleLeft className="w-6 h-6"></HiChevronDoubleLeft>
+        </IconButton>
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiChevronLeft className="w-6 h-6"></HiChevronLeft>
+        </IconButton>
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiChevronRight className="w-6 h-6"></HiChevronRight>
+        </IconButton>
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiChevronDoubleRight className="w-6 h-6"></HiChevronDoubleRight>
+        </IconButton>
+      </div>
+      <div className="flex flex-row gap-4 place-items-center justify-end">
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiMinus className="w-6 h-6"></HiMinus>
+        </IconButton>
+        <IconButton
+          color="deep-orange"
+          size="lg"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <HiPlus className="w-6 h-6"></HiPlus>
+        </IconButton>
+      </div>
+    </div>
   );
 }
 
@@ -126,10 +123,11 @@ export default function ReaderPage() {
   return (
     <>
       <NavigationBar />
-      <h1>me may beo</h1>
-      <h1>(bi anh huong style do o nhiem css tu file doc vao, can fix)</h1>
       <ReaderRenderer />
       <NavigationBar />
+      <h1>me may beo</h1>
+      <h1>(bi anh huong style do o nhiem css tu file doc vao, can fix)</h1>
+      <h1>^ (da fix) ko con bi anh huong do da xai dompurify de loc sach</h1>
     </>
   );
 }
